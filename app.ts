60b37a379b253936c0240065ee5be543e28ad46d
Emi <e.wong@hotmail.co.uk>
2021-10-25 14:57:01 +0100

    create basic interfaces

---
+++ app.ts
@@ -0,0 +1,15 @@
+interface Todo {
+  name: string;
+  completed?: boolean;
+}
+
+interface ITodoService {
+  add(todo: Todo): Todo;
+  delete(todo: Todo): void;
+  getAll(): Todo[];
+  getById(todoId: number): Todo;
+}
+
+var todo: Todo = {
+  name: 'Pick up drycleaning'
+};
